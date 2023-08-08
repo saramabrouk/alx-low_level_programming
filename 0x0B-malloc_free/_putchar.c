6be50000a637141881b649c3e66e9d8804e29aaa
saramabrouk <saramabrouk008@gmail.com>
2023-08-08 16:30:10 -0700

    malloc

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - write the character c to stoud
- * @c: the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: On successs 1
- * on error, -1 is returned, and errno is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-return(write(1, &c, 1));
+return (write(1, &c, 1));
 }
